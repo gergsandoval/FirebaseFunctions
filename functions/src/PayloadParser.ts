d3d743785a662a89682467d45081c3ea06bb62ce
German Sandoval <gsandoval@hexacta.com>
2020-10-17 02:10:09 -0300

    include top and bottom and the levels

--- functions/src/PayloadParser.ts
+++ functions/src/PayloadParser.ts
@@ -53,7 +53,7 @@ export default class PayloadParser {
 
     private static addMessageRef(hash: IHash): IHash {
         const severity: IHash | undefined = Object.values(<IHash []>hash["levels"])
-                                                  .find(level => level["min"] <= hash["value"] && level["max"] > hash["value"]); 
+                                                  .find(level => level["min"] <= hash["value"] && level["max"] >= hash["value"]); 
         if (!severity) throw new Error(`There's no mapped level of severity for property ${hash["property"]} and value ${hash["value"]} in Firebase Properties Collection`)
         hash["messageRef"] = severity["messageRef"];
         delete hash["levels"];
